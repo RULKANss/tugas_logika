f20f2d012b797e1cb90f662dd77eb804b46dd1c5
RULKANss <abitekor07@gmail.com>
2025-03-24 14:20:31 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,35 @@
+# Nomer 1
+p = True
+q = False
+hasil_1 = p or not q
+print(f"Soal 1: {hasil_1}")
+
+# Nomer 2
+p = True
+q = False
+hasil_2 = p and q
+print(f"Soal 2: {hasil_2}")
+
+# Nomer 3
+p = False
+q = True
+hasil_3 = (not p) or q
+print(f"Soal 3: {hasil_3}")
+
+# Nomer 4
+p = True
+hasil_4 = not p
+print(f"Soal 4: {hasil_4}")
+
+# Nomer 5
+p = True
+q = False
+r = True
+hasil_5 = p <= (q or r)
+print(f"Soal 5: {hasil_5}")
+
+# Nomer 6
+p = True
+q = True
+hasil_6 = (not p) and q
+print(f"Soal 6: {hasil_6}")
